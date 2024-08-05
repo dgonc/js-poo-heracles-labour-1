2be6f2429333f4bc1a67dbb810bdcb2f3ce1a449
dgonc <d.goncalves89000@gmail.com>
2024-08-05 10:57:16 +0200

    first work bases done, create fighters

--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,13 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strenght, dexterity) {
+    this.name = name;
+    this.strenght = strenght;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+}
+
+module.exports = Fighter;
